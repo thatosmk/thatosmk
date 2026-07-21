336872230c8d2b33adf6d73fd93be1f0130254ad
Thato Semoko <20558464+thatosmk@users.noreply.github.com>
2026-07-21 21:53:25 +0200

    Updates the landing page hero

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Thato Semoko';
+export const SITE_DESCRIPTION =
+	'Software engineer, founder, and tech lead based in Johannesburg.';
